c23cc29a7967ff3f6f1a85f2194bcb2487939a45
David Alvarez <david.alvarezz@gmail.com>
2023-08-25 00:44:19 -0400

    feat(S3): List Objects V2

--- s3/02-list-objects/main.go
+++ s3/02-list-objects/main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"context"
-	// "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	// "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"log"
 )
 
 func main() {
 	profile := "training-dev"
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile))
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile(profile))
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(cfg)
+
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{Bucket: aws.String("datasync-dev")})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Bucket name: %s", *output.Name)
+	log.Printf("first page results:\n")
+	for _, object := range output.Contents {
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+	}
 
 }
